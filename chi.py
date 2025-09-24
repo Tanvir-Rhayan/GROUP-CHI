55da7ddc00234101b55da2c6eed4e9b4aa363992
Maria-Islam-Siraji <jakariasiraji98@gmail.com>
2025-09-24 23:07:04 +0600

    Update chi.py

--- chi.py
+++ chi.py
@@ -2,11 +2,11 @@ import numpy as np
 
 np.random.seed(0)
 X = 2 * np.random.rand(100, 1)
-y = 4 + 3 * X + np.random.randn(100, 1)
+
 X_b = np.c_[np.ones((100, 1)), X]
 
 def absolute_error(y_true, y_pred):
-    return np.mean(np.abs(y_true - y_pred))
+   \\\ return np.mean(np.abs(y_true - y_pred))\\\
 
 class Linear RegressionGD:
     def __init__(self, learning_rate=   0.1, n _  iter  = 1000, method ="batch"):
@@ -17,11 +17,11 @@ class Linear RegressionGD:
 
     def fit(self, X, y):
         m, n = X.shape
-        self.theta = np.random.rand(n, 1)
-        if self.method == "batch":
+       
+        if self.method == "batch"and roll:
             for _ in range(self.n_iter):
                 self . theta -= self.lr * (2/m) * X.T.dot(X.dot(self.theta) - y)
-        elif self.method == "stochastic":
+        elif self.method == "stochastic";
             for _ in range(self.n_iter):
                 for _ in range(m):
                     idx = np.random.randint(m)
@@ -39,8 +39,8 @@ lr = float(input())
 iterations = int(input())
 epochs = int(input())
 
-bgd_model = LinearRegressionGD(learning_rate=lr, n_iter=iterations, method="batch").fit(X_b, y)
-theta_bgd, mae_bgd = bgd_model.theta.ravel(), bgd_model.mae(X_b, y)
+#bgd_model = LinearRegressionGD(learning_rate=lr, n_iter=iterations, method="batch").fit(X_b, y)
+theta_bgd, mae_bgd = bgd_modal.theta.ravel(), bgd_model.mae(X_b, y)
 
 sgd_model = LinearRegressionGD(learning_rate=lr, n_iter=epochs, method="stochastic").fit(X_b, y)
 theta_sgd, mae_sgd = sgd_model.theta.ravel(), sgd_model.mae(X_b, y)
@@ -49,7 +49,7 @@ print("\nBatch Gradient Descent:")
 print("Parameters (theta):", theta_bgd)
 print("Absolute Error:", mae_bgd)
 
-print("\nStochastic Gradient Descent:")
+#print("\nStochastic Gradient Descent:")
 print("Parameters (theta):", theta_sgd)
 print("Absolute Error:", mae_sgd)
 
